564540fe9b65e48a2fa5ef1d1b435e41acdb3af9
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-19 20:52:09 -0300

    Adicionando Listas e rotas.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,7 +15,23 @@
     </v-app-bar>
 
     <v-navigation-drawer app temporary class="primary" v-model="drawer">
-      <p>teste</p>
+      <v-list>
+        <v-list-item
+          v-for="link in links"
+          :key="link.icon"
+          router
+          :to="link.route"
+        >
+          <v-list-item-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{
+              link.text
+            }}</v-list-item-title></v-list-item-content
+          >
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -25,6 +41,11 @@ export default {
   data() {
     return {
       drawer: false,
+      links: [
+        { icon: "mdi-view-dashboard", text: "Dashboard", route: "/" },
+        { icon: "mdi-account", text: "My Projects", route: "/projects" },
+        { icon: "mdi-folder", text: "Team", route: "/team" },
+      ],
     };
   },
 };
