52e7d1e65a8d60d35a43064a187bfae7785a9c6a
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-19 13:57:22 -0300

    Textos e tipográfia.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="home"><h1>Home Page</h1></div>
+  <div class="home">
+    <h1>Home Page</h1>
+    <p class="blue white--text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, iste
+      excepturi maxime adipisci fugiat quo recusandae ipsam voluptas magni nulla
+      reiciendis unde eius praesentium aliquid tenetur asperiores quam sapiente!
+      Quos!
+    </p>
+    <p class="blue lighten-4 red--text text--darken-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, iste
+      excepturi maxime adipisci fugiat quo recusandae ipsam voluptas magni nulla
+      reiciendis unde eius praesentium aliquid tenetur asperiores quam sapiente!
+      Quos!
+    </p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1 text-uppercase">Massive Display</h4>
+    <p class="headline font-weight-bold">This is a headline</p>
+    <p class="subheading">this is a subheading</p>
+    <p class="caption">this is a caption</p>
+  </div>
 </template>
 
 <script>
