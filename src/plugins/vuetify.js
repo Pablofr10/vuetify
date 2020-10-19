0eb72ff20c059b79f43e5576f06c32db0cf0c6b6
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-19 13:39:04 -0300

    Modificando os componentes

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,18 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import { colors } from 'vuetify/lib';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      dark: {
+        primary: colors.yellow,
+        background: colors.yellow,
+      },
+    },
+  },
+});
+
+export default vuetify;
