d10b3894d03069a77342953f786ff27a331db6d6
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-19 16:28:21 -0300

    Toolbar e navigation.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="home">
     <h1>Home Page</h1>
-    <!-- <v-btn class="hidden-sm-and-down">Clique aqui</v-btn> -->
-    <!-- <v-btn class="hidden-md-and-up">Clique aqui</v-btn> -->
-    <v-btn class="hidden-xs-only">Click me</v-btn>
   </div>
 </template>
 
