3e150a2af5082cc9babbbdb75a81a7b907f5efe3
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-20 19:50:45 -0300

    Cards

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,13 +1,50 @@
 <template>
   <div class="team">
     <h1 class="subtitle-1 grey--text">Team</h1>
-    <v-container class="my-5"> content </v-container>
+    <v-container class="my-5">
+      <v-row wrap>
+        <v-col
+          cols="12"
+          xs="12"
+          sm="6"
+          md="4"
+          lg="3"
+          v-for="person in team"
+          :key="person.name"
+        >
+          <v-card elevation="1" class="text-center ma-3">
+            <v-responsive class="pt-4"> Images go here </v-responsive>
+            <v-card-text>
+              <div class="subheading">{{ person.name }}</div>
+              <div class="grey--text">{{ person.role }}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="blue">
+                <v-icon small left> mdi-message </v-icon>
+                <span>Message</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
   name: "Team",
+  data() {
+    return {
+      team: [
+        { name: "The Net Ninja", role: "Web developer" },
+        { name: "Ryu", role: "Graphic designer" },
+        { name: "Chun Li", role: "Web developer" },
+        { name: "Gouken", role: "Social media maverick" },
+        { name: "Yoshi", role: "Sales guru" },
+      ],
+    };
+  },
 };
 </script>
 
