9ffbc6e2f51147e51bb64a1f11c19bc2beb89963
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-19 23:23:38 -0300

    Grid parte 2

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,28 +2,26 @@
   <div class="dashboard">
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
     <v-container class="my-5">
-      <v-row wrap>
-        <v-col cols="12" md="6" xs="12">
-          <v-btn outlined block color="primary">1</v-btn>
-        </v-col>
-        <v-col cols="12" xs="12" md="2">
-          <v-btn outlined block color="primary">1</v-btn>
-        </v-col>
-        <v-col cols="12" xs="12" md="2">
-          <v-btn outlined block color="primary">1</v-btn>
-        </v-col>
-        <v-col cols="12" xs="12" md="2">
-          <v-btn outlined block color="primary">1</v-btn>
-        </v-col>
-      </v-row>
-      <v-row class="justify-space-between">
-        <v-col cols="12" sm="3" xs="12" md="2">
-          <v-btn outlined block color="primary">1</v-btn>
-        </v-col>
-        <v-col cols="12" sm="3" xs="12" md="2">
-          <v-btn outlined block color="primary">1</v-btn>
-        </v-col>
-      </v-row>
+      <v-card elevation="2" outlined class="pa-3">
+        <v-row>
+          <v-col md="6" xs="12" cols="12">
+            <div class="caption grey--text">Project Title</div>
+            <div>Create a new website</div>
+          </v-col>
+          <v-col xs="12" sm="4" md="2" cols="12">
+            <div class="caption grey--text">Person</div>
+            <div>The Net Ninja</div>
+          </v-col>
+          <v-col xs="6" sm="4" md="2" cols="12">
+            <div class="caption grey--text">Due by</div>
+            <div>1st Jan 2019</div>
+          </v-col>
+          <v-col xs="6" sm="4" md="2" cols="12">
+            <div class="caption grey--text">Status</div>
+            <div>ongoing</div>
+          </v-col>
+        </v-row>
+      </v-card>
     </v-container>
   </div>
 </template>
